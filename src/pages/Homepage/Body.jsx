5dedb9f1489322b2386a4cfe2787b04652b1d606
Yvonne Farhani Armelia Arman <fonny.armelia@gmail.com>
2020-05-12 03:01:20 +0700

    Add homepage design

--- src/pages/Homepage/Body.jsx
+++ src/pages/Homepage/Body.jsx
@@ -2,7 +2,10 @@ import React /* Fragment */ from 'react';
 import './homepage.css';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import people from './people.png';
+import rhino from './rhino.png';
 
 const useStyles = makeStyles((theme) => ({
 	base: {
@@ -33,7 +36,20 @@ const useStyles = makeStyles((theme) => ({
 		textAlign: 'right',
 		padding: '30px',
 		backgroundColor: 'transparent',
-		alignSelf: 'flex-end'
+		alignSelf: 'flex-end',
+		justifyContent: 'flex-end',
+	},
+	text3: {
+		fontFamily: 'Fredoka One',
+		fontStyle: 'normal',
+		fontWeight: 'normal',
+		fontSize: '36px',
+		lineHeight: '44px',
+		textAlign: 'right',
+		padding: '30px',
+		backgroundColor: 'transparent',
+		alignSelf: 'flex-end',
+		justifyContent: 'flex-end',
 	},
 	flexing: {
 		display: 'flex',
@@ -61,26 +77,60 @@ export default function Body() {
 					</Grid>
 
 					<Grid container>
-						<Grid item xs={6}>
-							{/* Ganti codingan yg ini kak */}
+						<Grid item xs={6} md={6} lg={6}>
 							<Paper elevation={0} className={classes.text2}>
-								Meet and Feed
-								<br />
-								The Wildlife,
-								<br />
-								From Your Screen.
+								<Grid container 
+								style={{fontSize: '22px',
+								lineHeight: '18px',
+								textAlign: 'right',
+								background: 'rgba(255, 253, 99, 0.6)',
+								padding: '20px',
+								borderRadius: '5px',
+								}}>
+									<Grid item xs={12} md={12} lg={6}
+									style={{textAlign: 'center', background: '#C4C4C4'}}><p><b>10</b> Cooperating zoos and conservations</p>
+									<Typography
+									style={{fontFamily: 'Raleway', fontSize: '18px'}}>
+										Educate
+									</Typography>
+									<img src={people} alt="people_png" style={{width: '100px'}} />
+									<Typography
+									style={{fontFamily: 'Raleway', fontSize: '18px'}}>
+										People
+									</Typography>
+									</Grid>
+									<br />
+									<Grid item xs={12} md={12} lg={6}
+									style={{textAlign: 'center', background: '#C4C4C4'}}><p><b>100</b> and more animals in shelter and protection</p>
+									<Typography
+									style={{fontFamily: 'Raleway', fontSize: '18px'}}>
+										Protect
+									</Typography>
+									<img src={rhino} alt="rhino_png" style={{width: '100px'}} />
+									<Typography
+									style={{fontFamily: 'Raleway', fontSize: '18px'}}>
+										Animals
+									</Typography>
+									</Grid>
+	
+								</Grid>
 							</Paper>
 						</Grid>
 
 						<Grid item xs={6}>
-							<Paper elevation={0} className={classes.text2}>
-								Meet and Feed
-								<br />
-								The Wildlife,
-								<br />
-								From Your Screen.
-							</Paper>
+							<Grid container>
+								<Grid item xs={12}>
+									<div className={classes.text3}>
+										Feed and Meet
+										<br />
+										The Wildlife,
+										<br />
+										From Your Screen.
+									</div>
+								</Grid>
+							</Grid>
 						</Grid>
+						
 					</Grid>
 				</div>
 			</div>
