12a369ba859048d6236a9e9cbab739f1c5ec40a0
fairusazil <fairus.azil@gmail.com>
2020-05-16 02:03:07 +0700

    addGendDon Page

---
+++ src/pages/GeneralDonation/BodyGenDon.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+
+
+
+function BodyGenDon() {
+    return (
+        <div>
+            <Grid>
+
+            </Grid>
+        </div>
+    )
+}
+
+export default BodyGenDon
---
+++ src/pages/GeneralDonation/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MenuGenDon from '../Components/MenuGenDon'
+import Body from "./Body";
+import Footer from "../Components/Footer";
+
+export default function Homepage() {
+  return (
+    <>
+      <MenuGenDon />
+      <Body />
+      <Footer />
+    </>
+  );
+}
