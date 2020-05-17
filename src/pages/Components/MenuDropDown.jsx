6fa11db5949d91a8fc01d1e18b71a25d87aaa84c
fairusazil <fairus.azil@gmail.com>
2020-05-18 01:05:46 +0700

    Add New Menu Dropdown

--- src/pages/Components/MenuDropDown.jsx
+++ src/pages/Components/MenuDropDown.jsx
@@ -6,14 +6,26 @@ import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-
-import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 import { Link } from 'react-router-dom';
 
 export default function SimpleMenu() {
 	const [anchorEl, setAnchorEl] = React.useState(null);
-	const handleClick = (event) => {
+	const [openCons, setOpenCons] = React.useState(false);
+	const [openWays, setOpenWays] = React.useState(false);
+	const handleClickCons = () => {
+		setOpenCons(!openCons);
+	};
+	const handleClickWays = () => {
+		setOpenWays(!openWays);
+	};
+	const handleClickMenu = (event) => {
 		setAnchorEl(event.currentTarget);
 	};
 	const handleClose = () => {
@@ -36,15 +48,19 @@ export default function SimpleMenu() {
 			color: '#6C5434',
 			background: '#60B6D1'
 		},
+		nested: {
+			paddingLeft: theme.spacing(4),
+		},
 	}));
+
 
 	const classes = useStyles();
 	return (
 		<div>
-			<Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+			<Button aria-controls="simple-Menu" aria-haspopup="true" onClick={handleClickMenu}>
 				<MenuIcon />
 			</Button>
-			<Menu id="simple-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
+			<Menu id="simple-Menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
 				<Link to="/sign-up" style={{ textDecoration: 'none' }}>
 					<MenuItem className={classes.fredokaFont} onClick={handleClose}>
 						Become a Member
@@ -56,16 +72,47 @@ export default function SimpleMenu() {
 					</MenuItem>
 				</Link>
 				<Divider />
-				<Link to="/zoos-and-conservation" style={{ textDecoration: 'none' }}>
-					<MenuItem className={classes.title} onClick={handleClose}>
-						Zoos and Conservation
-					</MenuItem>
-				</Link>
-				<Link to="/our-animals" style={{ textDecoration: 'none' }}>
-					<MenuItem className={classes.title} onClick={handleClose}>
-						Our Animals
-					</MenuItem>
-				</Link>
+				<ListItem button onClick={handleClickCons}>
+					Conservation
+								{openCons ? <ExpandLess /> : <ExpandMore />}
+				</ListItem>
+				<Collapse in={openCons} timeout="auto" unmountOnExit>
+					<List component="div" disablePadding>
+						<ListItem button className={classes.nested}>
+							<Link to="/zoos-and-conservation" style={{ textDecoration: 'none' }} className={classes.title}>
+									Zoos
+							</Link>
+						</ListItem>
+						<ListItem button className={classes.nested}>
+							<Link to="/our-animals" style={{ textDecoration: 'none' }} className={classes.title}>
+								Animals
+							</Link>
+						</ListItem>
+					</List>
+				</Collapse>
+				<ListItem button onClick={handleClickWays}>
+					Ways to Help
+								{openWays ? <ExpandLess /> : <ExpandMore />}
+				</ListItem>
+				<Collapse in={openWays} timeout="auto" unmountOnExit>
+					<List component="div" disablePadding>
+						<ListItem button className={classes.nested}>
+							<Link to="/general-donation" style={{ textDecoration: 'none' }} className={classes.title}>
+									Zoo Donation
+							</Link>
+						</ListItem>
+						<ListItem button className={classes.nested}>
+							<Link to="/feeding-animals" style={{ textDecoration: 'none' }} className={classes.title}>
+								Animal Feeding
+							</Link>
+						</ListItem>
+						<ListItem button className={classes.nested}>
+							<Link to="/animal-adopt" style={{ textDecoration: 'none' }} className={classes.title}>
+								Animal Adoption
+							</Link>
+						</ListItem>
+					</List>
+				</Collapse>
 				<Link to="#" style={{ textDecoration: 'none' }}>
 					<MenuItem className={classes.title} onClick={handleClose}>
 						Animals News
@@ -78,6 +125,6 @@ export default function SimpleMenu() {
 					</MenuItem>
 				</Link>
 			</Menu>
-		</div>
+		</div >
 	);
 }
