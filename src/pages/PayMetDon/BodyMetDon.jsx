168e7a5fac4e1273caff1810c71345d81ec78a60
fairusazil <fairus.azil@gmail.com>
2020-05-19 02:03:27 +0700

    formik on donation payment method

--- src/pages/PayMetDon/BodyMetDon.jsx
+++ src/pages/PayMetDon/BodyMetDon.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import Grid from '@material-ui/core/Grid'
-import Textarea from '@material-ui/core/TextareaAutosize'
+import TextArea from '@material-ui/core/TextareaAutosize';
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import Radio from '@material-ui/core/Radio'
@@ -12,6 +12,9 @@ import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
 import Link from '@material-ui/core/Link'
+
+import { Formik } from 'formik'
+import { useDispatch } from 'react-redux'
 
 const useStyle = makeStyles((theme) => ({
     root: {
@@ -30,15 +33,7 @@ const useStyle = makeStyles((theme) => ({
         fontSize: '44px',
         fontFamily: 'Fredoka One',
         color: '#6C5434',
-        // marginTop: theme.spacing(10)
-    },
-    ourAnimalsSm: {
-        display: 'flex',
-        textAlign: 'center',
-        fontSize: '34px',
-        fontFamily: 'Fredoka One',
-        color: '#6C5434',
-        // marginTop: theme.spacing(10)
+        marginTop: theme.spacing(10)
     },
     adoptForBetter: {
         fontFamily: 'Lemonada',
@@ -49,7 +44,7 @@ const useStyle = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         color: '#6C5434',
-        // marginTop: theme.spacing(1)
+        marginTop: theme.spacing(1)
     },
     giftText: {
         fontFamily: 'Raleway',
@@ -60,10 +55,10 @@ const useStyle = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         color: '#6C5434',
-        // marginTop: theme.spacing(1)
+        marginTop: theme.spacing(1)
     },
     margin: {
-        // margin: theme.spacing(3)
+        margin: theme.spacing(3)
     },
     text1: {
         fontFamily: 'Fredoka One',
@@ -83,52 +78,130 @@ const useStyle = makeStyles((theme) => ({
 
 export default function BodyMetDon() {
     const classes = useStyle();
+    const [value, setValue] = React.useState('');
+    const [value2, setValue2] = React.useState('');
 
+    const handleChange = (event) => {
+        setValue(event.target.value);
+    };
+    const handleChange2 = (event) => {
+        setValue2(event.target.value);
+    };
+
+    const submitData = (values)=>{
+        console.log(value, value2, values.name);
+        
+    }
     return (
         <div className={classes.root} style={{ background: '#ECE4BA' }}>
             <Grid container justify="center">
-
-                <Typography className={classes.ourAnimals}>Donate for our Zoo Partner</Typography>
-
-                <Typography className={classes.adoptForBetter}>Help them providing better place and facilities for animals</Typography>
+                <Typography className={classes.ourAnimals}>
+                    Donate for our Zoo Partner
+                    </Typography>
             </Grid>
+
+            <Grid container justify="center">
+                <Typography className={classes.adoptForBetter}>
+                    Help them providing better place and facilities for animals
+                    </Typography>
+            </Grid>
+
             <Grid container justify='center'>
-                <Grid item md={7} style={{ background: 'red', height: '100%', margin: '20px,', padding: '20px' }}>
-                    <Grid item md={12} style={{ padding: '10px' }}>
-                        Secure Order Form
+                <Formik
+                    initialValues={{
+                        name: '',
+                        email: '',
+                        message: '',
+                    }} onSubmit={async (values) => {
+                        submitData(values)
+                    }}
+                >
+                    {({ handleChange, handleSubmit, values }) => {
+                        return (
+
+
+                            <Grid item md={7} style={{ background: '#6C5434', height: '100%', margin: '20px,', padding: '20px' }}>
+                                <Grid item md={12} style={{ background: '#C4C4C4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px', color: '#6C5434' }}>
+                                    Secure Order Form
                     </Grid>
-                    <Grid item md={12} style={{ background: 'blue', padding: '10px' }}>
-                        <TextField label='Name' fullWidth style={{ background: '#C4C4C4' }} />
-                    </Grid>
-                    <Grid item md={12} style={{ background: 'blue', padding: '10px' }}>
-                        <TextField label='Email' fullWidth style={{ background: '#C4C4C4' }} />
-                    </Grid>
-                    <Grid item md={12} style={{ background: 'blue', padding: '10px' }}>
-                        <TextField label='Message for the Zookeepers' fullWidth style={{ background: '#C4C4C4' }} />
-                    </Grid>
-                    <Grid container>
-                        <Grid item md={12}>
-                            <RadioGroup>
-                                <FormControlLabel value='Anonymous' control={<Radio />} label='Use Anonymous'>
-                                </FormControlLabel>
-                                <Link style={{ textAlign: 'left' }}>Autofill with login data, click here to sign in.</Link>
-                            </RadioGroup>
-                        </Grid>
-                        <Grid item md={12}>
-                            <Button size='large' variant='outlined' style={{ margin: '10px' }}>Confirm</Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item md={4} style={{ background: 'green' }} height='100%' margin='20px'>
-                    <Grid item md={12} style={{ padding: '10px' }}>
+                                <form noValidate onSubmit={handleSubmit}>
+
+
+
+                                    <Grid item md={12} style={{ background: '#F6F4E4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px' }}>
+                                        <TextField id='name' name='name' values={values.name} onChange={handleChange} label='Name' fullWidth style={{ background: '#C4C4C4', textAlign: 'center' }} />
+                                    </Grid>
+
+                                    <Grid item md={12} style={{ background: '#F6F4E4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px' }}>
+                                        <TextField id='email' name='email' values={values.email} onChange={handleChange} label='Email' fullWidth style={{ background: '#C4C4C4', textAlign: 'center' }} />
+                                    </Grid>
+
+                                    <Grid item md={12} style={{ background: '#F6F4E4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px' }}>
+                                        <TextArea
+                                            id='message' name='message' values={values.message} onChange={handleChange}
+                                            placeholder="Message for the zookeeper"
+                                            style={{
+                                                background: '#C4C4C4',
+                                                textAlign: 'left',
+                                                fontFamily: 'Raleway', fontSize: '14px',
+                                                width: '50vw', height: '25vh'
+                                            }}></TextArea>
+                                    </Grid>
+
+                                    <Grid item md={12} style={{ background: '#F6F4E4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px' }}>
+                                        <RadioGroup>
+                                            <FormControlLabel value='Anonymous' control={<Radio />} label='Use Anonymous'>
+                                            </FormControlLabel>
+                                            <Link style={{ textAlign: 'left' }}>Autofill with login data, click here to sign in.</Link>
+                                        </RadioGroup>
+                                        <Grid item md={12}>
+                                            <Button type='submit' size="large" variant="outlined"
+                                                style={{ margin: '10px', color: '#6C5434' }}>
+                                                Confirm
+                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </form>
+                            </Grid>
+
+                        )
+                    }
+                    }
+                </Formik>
+                <Grid item md={4} style={{ background: '#6C5434', height: '100%', margin: '20px', padding: '20px' }}>
+                    <Grid item md={12} style={{ background: '#C4C4C4', fontFamily: 'Fredoka One', fontSize: '18px', padding: '10px', color: '#6C5434' }}>
                         Donate
                     </Grid>
-                    <Grid item md={12} style={{background:'yellow'}}>
-                        blabla
+                    <Grid item md={12} style={{ background: '#F6F4E4' }}>
+                        <FormControl component="fieldset" style={{ margin: '10px' }}>
+                            <FormLabel component="legend">Amount of donations</FormLabel>
+                            <RadioGroup aria-label="payment" name="gender1" value={value} onChange={handleChange}>
+                                <FormControlLabel value="harga1" control={<Radio />} label="Harga 1" />
+                                <FormControlLabel value="harga2" control={<Radio />} label="Harga 2" />
+                                <FormControlLabel value="harga3" control={<Radio />} label="Harga 3" />
+                                <FormControlLabel value="hargaRandom" control={<Radio />} label="Harga Random" />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item md={12} style={{ background: '#F6F4E4' }}>
+                        <FormControl component="fieldset" style={{ margin: '10px' }}>
+                            <FormLabel component="legend">Your payment is secured</FormLabel>
+                            <RadioGroup aria-label="payment" name="gender1" value={value2} onChange={handleChange2}>
+                                <FormControlLabel value="ATM" control={<Radio />} label="ATM Transfer" />
+                                <FormControlLabel value="CC" control={<Radio />} label="MasterCard / VISA" />
+                                <FormControlLabel value="OVO" control={<Radio />} label="OVO" />
+                                <FormControlLabel value="gopay" control={<Radio />} label="GoPay" />
+                            </RadioGroup>
+                        </FormControl>
+                        <Grid item md={12}>
+                            <Button size="large" variant="outlined"
+                                style={{ margin: '10px', color: '#6C5434' }}>
+                                Terms and Condition
+                            </Button>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-
         </div>
     )
 }
