56b695936c472c9c85868d928007a42babc85af0
targhib <targhib.ibrahim49@gmail.com>
2020-05-20 20:21:57 +0700

    fix bug

--- src/pages/AnimalsInfo/AnimalsBottomData.jsx
+++ src/pages/AnimalsInfo/AnimalsBottomData.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react';
 
 import { makeStyles, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyle = makeStyles((theme) => ({
 	image: {
@@ -9,12 +11,17 @@ const useStyle = makeStyles((theme) => ({
 		display: 'block',
 		maxWidth: '50%',
 		maxHeight: '10vh'
+	},
+	images: {
+		width: theme.spacing(4),
+		height: theme.spacing(4)
 	}
 }));
 
 export default function AnimalsBottomData(props) {
 	const classes = useStyle();
 	const currentAnimal = props.currAnimal.data;
+	console.log(currentAnimal);
 
 	return (
 		<Grid container>
@@ -27,24 +34,33 @@ export default function AnimalsBottomData(props) {
 			<Grid item lg={4}>
 				<Typography style={{ textAlign: 'left', fontFamily: 'Fredoka One' }}>Feeds</Typography>
 				<Typography style={{ textAlign: 'left', fontFamily: 'Raleway' }}>
-					{currentAnimal !== undefined ? currentAnimal.feeds : null}
+					{/* {currentAnimal !== undefined ? currentAnimal.feeds : null} */}
 				</Typography>
 			</Grid>
 			<Grid item lg={4}>
 				<Typography style={{ textAlign: 'left', fontFamily: 'Raleway' }}>Help these animals with</Typography>
-				<Typography style={{ textAlign: 'center', fontFamily: 'Raleway' }}>Provide the feeds or adopt them</Typography>
+				<Typography style={{ textAlign: 'center', fontFamily: 'Raleway' }}>
+					Provide the feeds or adopt them
+				</Typography>
 				<Grid container alignItems="center">
 					<Grid container item lg={5} alignItems="center">
-						<Grid item lg={5}>
-							<img
-								className={classes.image}
-								src="https://i.ibb.co/FBRSzPP/Pik-Png-com-fertilizer-png-4695532.png"
-								alt="Pik-Png-com-fertilizer-png-4695532"
-								border="0"
-							/>
-						</Grid>
-						<Grid item lg={6}>
-							<Typography style={{ textAlign: 'left', fontFamily: 'Fredoka One' }}>Feed Me</Typography>
+						<Grid item lg={12}>
+							<Button
+								variant="text"
+								size="small"
+								// onClick={() => handleClick()}
+								style={{ width: '100%', justifyContent: 'left' }}
+							>
+								<Avatar
+									alt=""
+									src="https://i.ibb.co/FBRSzPP/Pik-Png-com-fertilizer-png-4695532.png"
+									style={{ marginRight: '5%' }}
+									className={classes.images}
+								/>
+								<Typography variant="caption" style={{ fontFamily: 'Fredoka One', textAlign: 'left' }}>
+									Feed me
+								</Typography>
+							</Button>
 						</Grid>
 					</Grid>
 					<Grid item lg={2}>
@@ -52,16 +68,23 @@ export default function AnimalsBottomData(props) {
 						|{' '}
 					</Grid>
 					<Grid container item lg={5} alignItems="center">
-						<Grid item lg={5}>
-							<img
-								className={classes.image}
-								src="https://i.ibb.co/DMGt4Lc/kisspng-beagle-pet-sitting-pet-adoption-cat-pets-sign-5adccafe0d2a20-5333867415244193260539.png"
-								alt="kisspng-beagle-pet-sitting-pet-adoption-cat-pets-sign-5adccafe0d2a20-5333867415244193260539"
-								border="0"
-							/>
-						</Grid>
-						<Grid item lg={6}>
-							<Typography style={{ textAlign: 'left', fontFamily: 'Fredoka One' }}>Adopt Me</Typography>
+						<Grid item lg={12}>
+							<Button
+								variant="text"
+								size="small"
+								// onClick={() => handleClick()}
+								style={{ width: '100%', justifyContent: 'left' }}
+							>
+								<Avatar
+									alt=""
+									src="https://i.ibb.co/DMGt4Lc/kisspng-beagle-pet-sitting-pet-adoption-cat-pets-sign-5adccafe0d2a20-5333867415244193260539.png"
+									style={{ marginRight: '5%' }}
+									className={classes.images}
+								/>
+								<Typography variant="caption" style={{ fontFamily: 'Fredoka One', textAlign: 'left' }}>
+									Adopt me
+								</Typography>
+							</Button>
 						</Grid>
 					</Grid>
 				</Grid>
