ec01d1c03dd7a33d3f6e8f03e603357c2608c46a
targhib <targhib.ibrahim49@gmail.com>
2020-05-20 20:33:53 +0700

    turn off dev tools

--- src/index.js
+++ src/index.js
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from "redux";
-import allReducers from "./assets/redux/reducers";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from 'redux';
+import allReducers from './assets/redux/reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-const store = createStore (
-  allReducers,
-  compose(applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+const store = createStore(
+	allReducers,
+	compose(
+		applyMiddleware(thunk)
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	)
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
